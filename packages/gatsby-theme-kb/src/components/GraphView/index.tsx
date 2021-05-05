3920fd139526e9246a4e30013df04976e389b2de
hikerpig <hikerpigwinnie@gmail.com>
2021-05-05 22:52:16 +0800

    feat(gatsby-theme-kb): :sparkles: upgrade note-graph to support zooming to fit focused nodes in graph-view

--- packages/gatsby-theme-kb/src/components/GraphView/index.tsx
+++ packages/gatsby-theme-kb/src/components/GraphView/index.tsx
@@ -65,7 +65,7 @@ export default function GraphView({
     })
 
     if (currentFileId) {
-      noteGraphView.setSelectedNodes([currentFileId])
+      noteGraphView.setSelectedNodes([currentFileId], { shouldZoomToFit: true })
     }
 
     return () => {
